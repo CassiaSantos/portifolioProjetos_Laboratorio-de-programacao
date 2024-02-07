f564b10ccd8e911e868763033bba1c1c43097505
Cássia Oliveira dos Santos <86209186+CassiaSantos@users.noreply.github.com>
2024-02-07 18:14:51 -0300

    atualização em paradigmaDeclarativo_STR.py

--- ProcessamentoDeStringsComPython/paradigmaDeclarativo_STR.py
+++ ProcessamentoDeStringsComPython/paradigmaDeclarativo_STR.py
@@ -3,10 +3,12 @@ Professor: Cássio David Borralho Pinheiro;
 Curso: Sistemas de Informação - Turma de 2022;
 
 
-Este lgoritmo recebe uma string e um caractere e retorna o número de vezes que esse caractere aparece na string.
-Com ele, quero apresentar características do paradigma DECLARATIVO, o foco é a solução do problema, sem se 
-preocupar como será implemntada pelo computador. Isso é possível visualizar na linhas 22 através da chamada da 
-função count() nativa de python'''
+Este algoritmo recebe uma string e um caractere e retorna o número de vezes que esse caractere aparece na string.
+Com ele, quero apresentar características do paradigma DECLARATIVO, que tem como foco a solução do problema, 
+sem se preocupar como o passo a passo será implementado pelo computador. Essa caraterística pode ser vista na 
+linha 25, através da chamada da função count(), nativa de Python, pois ela conta quantas vezes um 
+determinado substring (ou caractere) ocorre na string principal e retorna o resultado. Logo, não é preciso que
+o programador especifique o passo a passo, somente chame a função.'''
 
 # ########################### PARADIGMA DECLARATIVO ###########################
 
