f74cbe0fd3e051587ffccb13171971c0fe185f76
Cássia Oliveira dos Santos <86209186+CassiaSantos@users.noreply.github.com>
2024-04-09 20:19:05 -0300

    adição de comentários no arquivo vogaisConsoantesTamanho.c

--- Avaliação02/vogaisConsoantesTamanho.c
+++ Avaliação02/vogaisConsoantesTamanho.c
@@ -1,13 +1,26 @@
+/* UNIVERSIDADE FEDERAL DO OESTE DO PARÁ
+Curso: Sistemas de Informação
+Professor: Cássio David Pinheiro
+Aluna: Cássia Oliveira dos Santos
+
+Nesse programa, busco calcular a quantidade de vogais, consoantes, espaço em branco e total de caracteres
+inseridos manualmente no código através do paradigma imperativo sem utilizar metódos da biblioteca string.h
+para exibir diretamente no console.*/
+
 #include <stdio.h>
 #include <string.h>
 
 void main() {
 
-    //Cálculo da quantidade de letras e consoante na palavra:
+    //defina a string a ser processada aqui:
     char palavra[] = "Conte A quantidade De Vogais E consoantes Nesta Frase";
+    
+    //variáveis necessárias:
     int qtVogais = 0, qtConsoantes = 0, espacoBranco = 0;
-
+    
+    //Cálculo da quantidade de vogais e consoante na string de entrada:
     for(int i=0; i < strlen(palavra); i++){
+        //caso o caracter atual for vogal:
         switch(palavra[i]) {
             case 'A':
             case 'a':
@@ -19,13 +32,17 @@ void main() {
             case 'o':
             case 'U':
             case 'u':
+                //será incrementado 1 em qtVogais:
                 qtVogais++;
                 break;
+            //caso contrário:
             default:
+                //a letra atual pode ser consoante:
                 qtConsoantes++;
         }
     }
 
+    //loop que percorre novamente a string e conta quantidade de espaços em branco:
     for(int i = 0; i < strlen(palavra); i++){
         switch(palavra[i]) {
             case ' ':
